bd0e622327a34639427f2d1aaaa7b8125f5b2a9f
mattroz <matveyrozanov@gmail.com>
2016-11-02 00:00:51 +0300

    Class for non-linear equations

---
+++ coursework/NonLinearEquation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coursework
+{
+    /// <summary>
+    /// Class for non-linear equations solving
+    /// </summary>
+    class NonLinearEquation
+    {
+        //  class fields
+        private float left_interval;
+        private float right_interval;
+        private float epsilon;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="left">Left root interval</param>
+        /// <param name="right">Right root interval</param>
+        /// <param name="eps">Approximation</param>
+        NonLinearEquation(float left, float right, float eps)
+        {
+            this.left_interval = left;
+            this.right_interval = right;
+            this.epsilon = eps;
+        }
+    }
+}
